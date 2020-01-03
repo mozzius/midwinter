40b0fade8a67407a63939bcc917388155941c71a
Samuel <mozzius@protonmail.com>
2020-01-03 16:09:46 +0000

    corrected yarn build and port

--- midwinter-api/index.js
+++ midwinter-api/index.js
@@ -1,5 +1,5 @@
 const { app } = require('./server')
 
-const port = process.env.NODE_ENV === 'production' ? 80 : 5000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Midwinter API has launched 🚀 (port: ${port})`))
